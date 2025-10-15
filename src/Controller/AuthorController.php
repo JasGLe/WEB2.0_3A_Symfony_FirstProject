4a6cf6f0c0d1b65b6bcce76d303c933c3635e9ca
Jasser Yahyaoui <jasseryahyaoui36@gmail.com>
2025-10-15 14:40:00 +0100

    feat: add,delete,update and findall features are now implemented in /AuthorController. formTypes are used now for updates and adds

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,45 +2,71 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AuthorController extends AbstractController
 {
-    #[Route('/author/{name}', name: 'app_author')]
-    public function showAuthor( string $name): Response
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
     {
-        return $this->render('author/show.html.twig', [
-            'name' => $name,
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
         ]);
     }
-    #[Route('/authors', name: 'app_authors')]
-    public function listAuthors()
-    {
-        $authors = array(
-            array('id' => 1, 'picture' => '/assets/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/assets/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
-            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-            array('id' => 3, 'picture' => '/assets/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300),);
+    #[Route("/add",name:"add")]
+    public function add(ManagerRegistry $doctrine,Request $request):Response{
+        $author = new  Author();
+        $em = $doctrine->getManager();
+        $form =  $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($author);
+            $em->flush();
+            return $this->redirectToRoute("findAll");
+        }
+        return $this->render("author/add.html.twig",[
+             'form' => $form->createView(),
+             ]);
+    }
 
-        return $this->render('author/list.html.twig', [
-            'authors' => $authors,
+    #[Route("/findAll",name : "findAll")]
+    public function findAll(AuthorRepository $repository):Response{
+        $authors = $repository->findAll();
+        return $this->render("author/authors.html.twig",[
+            "authors" => $authors
         ]);
     }
-    #[Route('/authors/{id}', name: 'app_author_details')]    
-    public function authorDetails($id){ 
-        $authors = array(
-            array('id' => 1, 'picture' => '/assets/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/assets/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
-            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
-            array('id' => 3, 'picture' => '/assets/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300),);
-        return $this->render('author/showAuthor.html.twig',['id' => $id            
-        ,'authors' => $authors,
+
+
+    #[Route("/update/{id}" , name :"update")]
+    public function update(ManagerRegistry $doctrine , $id , AuthorRepository $repository , Request $request):Response{
+        $author = $repository->find($id);
+        $em = $doctrine->getManager();
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+            return new Response("Author updated successfully");
+        }
+
+        return $this->render("author/update.html.twig",[
+            'form' => $form->createView(),
         ]);
+    }
+
+    #[Route("/delete/{id}" ,name : "delete")]
+    public function delete(ManagerRegistry $doctrine , AuthorRepository $repository , $id):Response{
+        $author = $repository->find($id);
+        $em = $doctrine->getManager();
+        $em->remove($author);
+        $em->flush();
+        return new Response("Author deleted successfully");
     }
 }
